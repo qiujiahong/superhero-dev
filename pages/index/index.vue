4b5e8f72f7899568b1ba9782018b8dd17a6b3fd3
qiujiahong <qiujiahongde@163.com>
2019-04-25 22:19:14 +0800

    修改箭头函数

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -19,16 +19,16 @@
 			}
 		},
 		onLoad() {
-			var me = this;
+			//var me = this;
 			//请求轮播图数据
 			uni.request({
 				url: 'https://www.imovietrailer.com/superhero/index/carousel/list?qq=309284701', //仅为示例，并非真实接口地址。
 				method: "POST",
 				success: (res) => {
 					if(res.data.status == 200){
-						var carouselList = res.data.data;
-						me.carouselList= res.data.data;
-						console.log(me.carouselList);
+						//var carouselList = res.data.data;
+						this.carouselList= res.data.data;
+						console.log(this.carouselList);
 					}
 				}
 			});
