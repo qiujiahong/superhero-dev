17ce1166123ece8f1cfae6cf268a1c9210a38815
qiujiahong <qiujiahongde@163.com>
2019-05-01 13:34:21 +0800

    动态查询搜索结果

--- pages/search/search.vue
+++ pages/search/search.vue
@@ -8,90 +8,50 @@
 		</view>
 		
 		<view class="movie-list page-block">
-			<view class="movie-wapper">
-				<image src="../../static/poster/civilwar.jpg" class="poster"></image>
+			<view class="movie-wapper" v-for="trailer in trailerList">
+				<image :src="trailer.cover" class="poster"></image>
 			</view>
 			
-			<view class="movie-wapper">
+			<!-- <view class="movie-wapper">
 				<image src="../../static/poster/civilwar.jpg" class="poster"></image>
-			</view>
-			<view class="movie-wapper">
-				<image src="../../static/poster/civilwar.jpg" class="poster"></image>
-			</view>
-			<view class="movie-wapper">
-				<image src="../../static/poster/civilwar.jpg" class="poster"></image>
-			</view>
-			
-		<view class="movie-wapper">
-			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
-		</view>
-		<view class="movie-wapper">
-			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
-		</view>
-		<view class="movie-wapper">
-			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
-		</view>
-		<view class="movie-wapper">
-			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
-		</view>
-		<view class="movie-wapper">
-			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
-		</view>
-		<view class="movie-wapper">
-			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
-		</view>
-			
-		<view class="movie-wapper">
-			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
-		</view>
-		<view class="movie-wapper">
-			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
-		</view>
-		<view class="movie-wapper">
-			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
-		</view>
-		<view class="movie-wapper">
-			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
-		</view>
-		<view class="movie-wapper">
-			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
-		</view>
-		<view class="movie-wapper">
-			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
-		</view>
-			
-		<view class="movie-wapper">
-			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
-		</view>
-		<view class="movie-wapper">
-			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
-		</view>
-		<view class="movie-wapper">
-			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
-		</view>
-		<view class="movie-wapper">
-			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
-		</view>
-		<view class="movie-wapper">
-			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
-		</view>
-		<view class="movie-wapper">
-			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
-		</view>
-		
+			</view> -->
 		</view>
 	</view>
 </template>
 
 <script>
+	import common from "../../common/common.js";
+	
 	export default {
 		data() {
 			return {
-				
+				trailerList:[]
 			}
 		},
 		methods: {
 			
+		},
+		onLoad() {
+			//查询猜你喜欢
+			uni.showLoading({
+				// 防止用户点击某一些按钮
+				mask:true,
+				title:"请稍后"
+			})
+			var serverUrl = common.serverUrl+ "/search/list?"+ common.qqStr+"&keywords=&page=&pageSize=";
+			uni.request({
+				url: serverUrl ,
+				method: "POST",
+				success: (res) => {
+					if(res.data.status == 200){
+						this.trailerList= res.data.data.rows;
+						console.log(this.trailerList)
+					}
+				},
+				complete:() => {
+					uni.hideLoading();
+				}
+			});
 		}
 	}
 </script>
