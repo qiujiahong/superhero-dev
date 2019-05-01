862a55d4791c8792c67c629f45e333406a5b8da7
qiujiahong <qiujiahongde@163.com>
2019-05-01 21:52:49 +0800

    添加详情的影片显示

--- pages/movie/movie.vue
+++ pages/movie/movie.vue
@@ -1,14 +1,22 @@
 <template>
-	<view>
-		这是电影预告片的详情页面
+	<view class="page">
+		<view class="player">
+			<video 
+				:src="tailerInfo.trailer" 
+				:poster="tailerInfo.poster"
+				class="movie"
+				controls></video>
+		</view>
 	</view>
 </template>
 
 <script>
+	import common from "../../common/common.js";
+	
 	export default {
 		data() {
 			return {
-				
+				tailerInfo:{}
 			}
 		},
 		methods: {
@@ -17,10 +25,23 @@
 		onLoad(params) {
 			//获取上一个页面传入的参数
 			var trailerId = params.trailerId;
+			//获取预告片的详细信息
+			//查询猜你喜欢
+			var serverUrl = common.serverUrl+ "/search/trailer/"+trailerId +"?qq="+common.qqStr;
+			uni.request({
+				url: serverUrl ,
+				method: "POST",
+				success: (res) => {
+					if(res.data.status == 200){
+						var tailerInfo= res.data.data;
+						this.tailerInfo = tailerInfo;
+					}
+				}
+			});
 		}
 	}
 </script>
 
 <style>
-
+@import url("movie.css");
 </style>
