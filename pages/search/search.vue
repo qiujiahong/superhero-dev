8f63f23d43ceb629f236164c1c819a20588b8374
qiujiahong <qiujiahongde@163.com>
2019-05-01 13:18:25 +0800

    完成搜索的静态代码

--- pages/search/search.vue
+++ pages/search/search.vue
@@ -6,6 +6,80 @@
 			</view>
 			<input type="text" placeholder="搜索预告" maxlength="10"  class="search-text" focus/>
 		</view>
+		
+		<view class="movie-list page-block">
+			<view class="movie-wapper">
+				<image src="../../static/poster/civilwar.jpg" class="poster"></image>
+			</view>
+			
+			<view class="movie-wapper">
+				<image src="../../static/poster/civilwar.jpg" class="poster"></image>
+			</view>
+			<view class="movie-wapper">
+				<image src="../../static/poster/civilwar.jpg" class="poster"></image>
+			</view>
+			<view class="movie-wapper">
+				<image src="../../static/poster/civilwar.jpg" class="poster"></image>
+			</view>
+			
+		<view class="movie-wapper">
+			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
+		</view>
+		<view class="movie-wapper">
+			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
+		</view>
+		<view class="movie-wapper">
+			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
+		</view>
+		<view class="movie-wapper">
+			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
+		</view>
+		<view class="movie-wapper">
+			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
+		</view>
+		<view class="movie-wapper">
+			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
+		</view>
+			
+		<view class="movie-wapper">
+			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
+		</view>
+		<view class="movie-wapper">
+			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
+		</view>
+		<view class="movie-wapper">
+			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
+		</view>
+		<view class="movie-wapper">
+			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
+		</view>
+		<view class="movie-wapper">
+			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
+		</view>
+		<view class="movie-wapper">
+			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
+		</view>
+			
+		<view class="movie-wapper">
+			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
+		</view>
+		<view class="movie-wapper">
+			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
+		</view>
+		<view class="movie-wapper">
+			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
+		</view>
+		<view class="movie-wapper">
+			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
+		</view>
+		<view class="movie-wapper">
+			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
+		</view>
+		<view class="movie-wapper">
+			<image src="../../static/poster/civilwar.jpg" class="poster"></image>
+		</view>
+		
+		</view>
 	</view>
 </template>
 
