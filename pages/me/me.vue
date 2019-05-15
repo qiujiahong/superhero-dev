ae2514eea3943bcd5cfce68cdb95dd3808c8b1ed
nick qiu <nick@nicks-MacBook-Pro.local>
2019-05-15 19:15:11 +0800

    未登录下的显示；

--- pages/me/me.vue
+++ pages/me/me.vue
@@ -1,9 +1,32 @@
 <template>
 	<view class="page page-fill">
 		<view class="header">
+			<view v-if="userIsLogin">
+				<image src="../../static/icos/108x108.png" class="face"></image>
+			</view>
+			<view v-else>
+				<image src="http://122.152.205.72:88/group1/M00/00/05/CpoxxFw_-5-AFyVyAABLIH8xBTw233.png" class="face"></image>
+			</view>
 			
+			<view class="info-wapper"  v-if="userIsLogin">
+				<view class="nickname">
+					张三
+				</view>
+				<view class="nav-info">
+					ID:1233445555
+				</view>
+			</view>
+			<view  v-else>
+				<view class="nickname regist-login" >
+					注册/登录
+				</view>
+			</view>
+			
+			<view class="set-wapper"  v-if="userIsLogin">
+				<image src="../../static/icos/settings.png" class="settings"></image>
+			</view>
 		</view>
-		
+
 	</view>
 </template>
 
@@ -11,11 +34,11 @@
 	export default {
 		data() {
 			return {
-				
+				userIsLogin: true
 			}
 		},
 		methods: {
-			
+
 		}
 	}
 </script>
